66afb7b3c3530d24a53a7b629b46de33a4d02b4c
Dinara Andreeva <dinara.andreyeva@phystech.edu>
2020-06-02 23:36:38 +0900

    collision method

--- game.py
+++ game.py
@@ -2,7 +2,19 @@ class Object:
     def __init__(self, xy, size):
         self.rect = Rect(xy, size)
     
-    def collide_borders(self, object2):
+    def collide_line_check(self, line):
+
+        clipped_line = self.clipline(line)
+
+        if clipped_line:
+            # If clipped_line is not an empty tuple then the line
+            # collides/overlaps with the rect. The returned value contains
+            # the endpoints of the clipped line.
+            return True
+        else:
+            return False
+
+    def collide_line_measure(self, line):
 
         clipped_line = self.clipline(line)
 
@@ -13,7 +25,4 @@ class Object:
             start, end = clipped_line
             x1, y1 = start
             x2, y2 = end
-        else:
-            print("No clipping. The line is fully outside the rect.")
-
-
+             
