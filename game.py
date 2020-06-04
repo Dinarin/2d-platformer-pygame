1a847e6011316019a998afe0bd9bfb9d6fc93b69
Dinara Andreeva <dinara.andreyeva@phystech.edu>
2020-06-04 18:48:32 +0900

    window opens

--- game.py
+++ game.py
@@ -1,6 +1,30 @@
 import sys, pygame
 import math
 import lib.objects as objects
+import lib.canvas as canvas
 
+# Initializing pygame
 pygame.init()
 
+# Temporary variables
+window_size = (800,600)
+canvas_size = (10,10)
+
+# Creating the main surface
+game_screen = pygame.display.set_mode(window_size)
+
+# Creating canvas
+canvas = canvas.Canvas(canvas_size)
+
+# Game cycle
+clock = pygame.time.Clock()
+tt = 0
+while True:
+    dt = clock.tick(50) /1000.0
+
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            sys.exit()
+    tt += dt
+    game_screen.blit(a, (400,400))
+    pygame.display.flip()
--- lib/canvas.py
+++ lib/canvas.py
@@ -7,6 +7,12 @@ class Canvas(pygame.Surface):
     ---------- 
     Methods
     ---------- 
+    __init__(
     """
-    def __init__():
-        self.screen =
+    def __init__(self, dimensions):
+        blue = (0,0,255)
+        pygame.Surface.__init__(self, dimensions)
+        self.fill(blue)
+
+class ObjectSprite(pygame.sprite.Sprite):
+    def __init__(self, coordinate
